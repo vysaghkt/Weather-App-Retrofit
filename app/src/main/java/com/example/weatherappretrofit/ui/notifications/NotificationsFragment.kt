aa26d2fbd8b37f10075feb9775fc19b3f1bfc3a0
VysaghKT <vysagh.kt@gadgeon.com>
2021-10-31 16:37:16 +0530

    Setup Spinner for selecting units and stored using DataStore

--- app/src/main/java/com/example/weatherappretrofit/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/weatherappretrofit/ui/notifications/NotificationsFragment.kt
@@ -4,17 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.weatherappretrofit.R
+import androidx.lifecycle.lifecycleScope
 import com.example.weatherappretrofit.databinding.FragmentNotificationsBinding
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.collect
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
     private var _binding: FragmentNotificationsBinding? = null
+
+    private lateinit var storeDataRepository: StoreDataRepository
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,18 +26,53 @@ class NotificationsFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
-
+    ): View {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        storeDataRepository = context?.let { StoreDataRepository(it) }!!
+
+        lifecycleScope.async {
+            storeDataRepository.readUnits().collect {
+                setSelectedOptionSpinner(it)
+            }
+        }
+
+
         return root
+    }
+
+    private fun setSelectedOptionSpinner(unit: String) {
+
+        val unitsList = if (unit == "Metric" || unit == "") {
+            listOf("Metric", "Imperial")
+        } else {
+            listOf("Imperial", "Metric")
+        }
+
+        val arrayAdapter =
+            ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, unitsList)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = arrayAdapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                lifecycleScope.async {
+                    storeDataRepository.storeUnits(parent?.getItemAtPosition(position).toString())
+
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/weatherappretrofit/ui/notifications/StoreDataRepository.kt
@@ -0,0 +1,28 @@
+package com.example.weatherappretrofit.ui.notifications
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.map
+
+class StoreDataRepository(private val context: Context) {
+
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("my_preference")
+
+    companion object {
+        val UNITS = stringPreferencesKey("units")
+    }
+
+    suspend fun storeUnits(unitType: String) {
+        context.dataStore.edit {
+            it[UNITS] = unitType
+        }
+    }
+
+    fun readUnits() = context.dataStore.data.map {
+        it[UNITS] ?: ""
+    }
+}
